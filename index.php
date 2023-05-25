42d973f61ff0907be356cdad10d4edd3b959b21f
StenP6lts <130351136+StenP6lts@users.noreply.github.com>
2023-05-25 18:22:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.container {
-  position: relative;
-  text-align: center;
-  color: white;
-}
-.centered {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
-</style>
-</head>
-<body>
-
-<h2>Tere tulemast</h2>
-<p>Steni veebileht:</p>
-
-<div class="container">
-  <img src="dog.jpg" alt="Snow" style="width:100%;">
-  <div class="bottom-left">Bottom Left</div>
-  <div class="top-left">Top Left</div>
-  <div class="top-right">Top Right</div>
-  <div class="bottom-right">Bottom Right</div>
-  <div class="centered">Centered</div>
-</div>
-
-</body>
-</html> 
+  <head>
+    <title>Pretty Paris</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="image">
+        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg">
+      </div>
+      <div class="text">
+        <h1>Tere tulemast.</h1>
+      </div>
+    </div>
+  </body>
+</html>
