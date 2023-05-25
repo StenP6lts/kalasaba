a5e8af9dff8182030687c31645d090fe3d1ae556
StenP6lts <130351136+StenP6lts@users.noreply.github.com>
2023-05-25 18:00:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
-</head>
-<body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
-</body>
-</html>
+<style>
+.left, .right {
+  float: left;
+  width: 20%; /* The width is 20%, by default */
+}
+
+.main {
+  float: left;
+  width: 60%; /* The width is 60%, by default */
+}
+
+/* Use a media query to add a breakpoint at 800px: */
+@media screen and (max-width: 800px) {
+  .left, .main, .right {
+    width: 100%; /* The width is 100%, when the viewport is 800px or smaller */
+  }
+}
