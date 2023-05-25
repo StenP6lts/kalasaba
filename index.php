7b33f536c8c93cc98c7818a029f9f5a3a818ac7e
StenP6lts <130351136+StenP6lts@users.noreply.github.com>
2023-05-25 18:08:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
-<style>
-.left, .right {
-  float: left;
-  width: 20%; /* The width is 20%, by default */
-}
-
-.main {
-  float: left;
-  width: 60%; /* The width is 60%, by default */
-}
-
-/* Use a media query to add a breakpoint at 800px: */
-@media screen and (max-width: 800px) {
-  .left, .main, .right {
-    width: 100%; /* The width is 100%, when the viewport is 800px or smaller */
-  }
-}
+<div class="example">
+  <div class="overlay-text">
+    <h1>Tere tulemast</h1>
+  </div>
+</div>
