d8a4fe54571f1769c7ca3e81a5b3472328338f61
StenP6lts <130351136+StenP6lts@users.noreply.github.com>
2023-05-25 18:25:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg">
       </div>
       <div class="text">
-        <h1>Tere tulemast.</h1>
+        <h1>Tere tulemast</h1>
       </div>
     </div>
   </body>
