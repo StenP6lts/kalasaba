ad04b1a0b943ad5891bfe2f5885885d33260d10f
StenP6lts <130351136+StenP6lts@users.noreply.github.com>
2023-05-25 18:13:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
-<div class="example">
-  <div class="overlay-text">
-    <h1>Tere tulemast</h1>
-  </div>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.container {
+  position: relative;
+  text-align: center;
+  color: white;
+}
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
+</head>
+<body>
+
+<h2>Image Text</h2>
+<p>How to place text over an image:</p>
+
+<div class="container">
+  <img src="img_snow_wide.jpg" alt="Snow" style="width:100%;">
+  <div class="bottom-left">Bottom Left</div>
+  <div class="top-left">Top Left</div>
+  <div class="top-right">Top Right</div>
+  <div class="bottom-right">Bottom Right</div>
+  <div class="centered">Centered</div>
 </div>
+
+</body>
+</html> 
